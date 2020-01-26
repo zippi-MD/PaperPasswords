a64892b501212b884e6e604027fe82c29c84bb24
Alejandro Mendoza <zippi180@gmail.com>
2020-01-26 17:32:27 -0600

    Added missing UI elements to show cards generator properties

--- PPP-Crypto-Proyect/Controller/ConfigureViewController.swift
+++ PPP-Crypto-Proyect/Controller/ConfigureViewController.swift
@@ -7,18 +7,38 @@
 //
 
 import UIKit
+import CryptoKit
 
 class ConfigureViewController: UIViewController {
 
     @IBOutlet weak var sequenceKeyLabel: UILabel!
     @IBOutlet weak var generateSequenceKeyButton: UIButton!
     @IBOutlet weak var generateCardsButton: UIButton!
+    @IBOutlet weak var passwordCharacterSetTextView: UITextView!
+    @IBOutlet weak var passwordLength: UILabel!
+    
+    var sequenceKey: SymmetricKey?
+    
+    let suggestedPasswordCharacterSet = "!#%+23456789:=?@ABCDEFGHJKLMNPRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupUI()
+    }
+    
+    func setupUI(){
+        passwordCharacterSetTextView.text = suggestedPasswordCharacterSet
     }
     
 
-
+    @IBAction func generateKeyTapped(_ sender: UIButton) {
+        sequenceKey = generateSequenceKey()
+        sequenceKeyLabel.text = sequenceKey?.withUnsafeBytes { Data(Array($0)).base64EncodedString() }
+    }
+    
+    @IBAction func changePasswordLengthTapped(_ sender: UIStepper) {
+        passwordLength.text = "\(Int(sender.value))"
+    }
+    
 }
